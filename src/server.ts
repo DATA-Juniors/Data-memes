597653ffd5bddf335f2397acb49a9c9a1e503a6d
Amir <amiryusupov.070@gmail.com>
2023-06-18 18:07:27 +0500

    connect with db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
-app.listen(8080, () => {
-    console.log("Server is running on http://localhost:8080")
+const port = process.env.SERVER_PORT
+    
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
 })
